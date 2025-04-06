ca37c6e932b730e06a9aaa73ae83d9a216bbbe9f
Mariemomezzine <mariem.omezzine@esprit.tn>
2025-04-06 19:02:29 +0100

    first commit

---
+++ src/test/java/com/example/RentaGo/RentaGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RentaGo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentaGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
