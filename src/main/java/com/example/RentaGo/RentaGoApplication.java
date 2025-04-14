4ec20925f9a3a883c68d9e3e4031de9b7241f90e
olva7 <olfa.kacem@etudiant-enit.utm.tn>
2025-04-14 19:50:31 +0100

    First commit

---
+++ src/main/java/com/example/RentaGo/RentaGoApplication.java
@@ -0,0 +1,15 @@
+package com.example.RentaGo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentaGoApplication {
+
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentaGoApplication.class, args);
+	}
+
+}
