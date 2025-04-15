bf9491465c6c7b3c77151586def1b1c0d9579771
olva7 <olfa.kacem@etudiant-enit.utm.tn>
2025-04-15 20:46:36 +0100

    test

--- src/main/java/com/example/RentaGo/RentaGoApplication.java
+++ src/main/java/com/example/RentaGo/RentaGoApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RentaGoApplication {
 
-
-
 	public static void main(String[] args) {
 		SpringApplication.run(RentaGoApplication.class, args);
 	}
